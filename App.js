acdeb8e3c232ba2e8ed9ebe13f3bb9de9bb1b3bd
MohammedQuraysh <mohammedquraysh4@gmail.com>
2021-11-30 17:48:03 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const[number, setNumber] = useState(0)
+  const[count, setCount] = useState(0)
+  const[name, setName] = useState('')
+  const addClick = ()=>{
+    setNumber(number + 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>The number is {number}</Text>
+      <Button title='add' onPress={addClick}></Button>
+      <Text>You clicked me {count} times</Text>
+      <Button title='click me' onPress={()=>{setCount(count + 5)}}></Button>
+      <Text>You clicked me {number} times</Text>
+      <TextInput style={styles.textInput} placeholder="Enter your name" onChangeText={text=> setName(text)}/>
+      <Text>my name is {name}</Text>
     </View>
   );
 }
@@ -18,4 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput:{
+    margin: 20
+  }
 });
